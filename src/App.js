cda2619559c0b488c8589718093902614c0baa22
Vikram Mandloi <111233496+V-mandloi@users.noreply.github.com>
2023-09-04 23:59:57 +0530

    This is my second task from Forage
    I added some banner with tile name Sextant and a block of text.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
 
-export default App;
+const Banner = () => {
+  return (
+    <div>
+      <div className="banner">
+        <h1>Sextant</h1>
+      </div> 
+      <div className="exhibit">
+        <h1 className="exhibit-title">Sextant</h1>
+        <h1 className="exhibit-title">"Hey there, I'm vikram, a budding web developer eager to share my journey with you. In the present, I'm passionately learning the intricacies of web development, soaking up knowledge like a sponge. In the past, I laid the foundation, mastering HTML, CSS, and JavaScript.
+
+Looking ahead, my future goals are clear: I aspire to specialize in front-end development, crafting visually stunning and user-friendly websites. Beyond that, I aim to delve into back-end development and database management to create full-stack solutions. Together, let's witness my growth from novice to expert as I navigate this exciting realm of web development."</h1>
+      </div>     
+    </div>
+
+     );
+  
+};
+
+export default Banner;
